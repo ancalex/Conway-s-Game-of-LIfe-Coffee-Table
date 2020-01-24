6ee971a3b41d4ef26e3dbaa2f7f91f31bc71f4a3
ancalex <andrei.erdei@gmail.com>
2020-01-24 08:02:41 +0200

    Update Conway_s_Game_of_Life_Coffee_Table.ino

--- Conway_s_Game_of_Life_Coffee_Table.ino
+++ Conway_s_Game_of_Life_Coffee_Table.ino
@@ -6,7 +6,7 @@
 #define FASTLED_INTERRUPT_RETRY_COUNT 0
 #include "FastLED.h"
 
-#define LED_TYPE    WS2811
+#define LED_TYPE WS2811
 #define COLOR_ORDER GRB
 #define NUM_STRIPS 5
 #define NUM_LEDS_PER_STRIP 125
@@ -35,15 +35,12 @@ Cell world[MatrixWidth][MatrixHeight];
 #include "global.h"
 #include "gradients.h"
 #include "NTP.h"
-//#include "SunCalculator.h"
-//#include "MainFunctions.h"
 
 // Include STYLE and Script "Pages"
 #include "Page_Script.js.h"
 #include "Page_Style.css.h"
 
 // Include HTML "Pages"
-//#include "Page_ScheduleSettings.h"
 #include "Page_Admin.h"
 #include "Page_NTPSettings.h"
 #include "Page_Information.h"
@@ -82,7 +79,7 @@ void setup() {
 	if ((WIFI_connected != WL_CONNECTED) or !CFG_saved) {
 		// DEFAULT CONFIG
 		Serial.println("Setting AP mode default parameters");
-		config.ssid = "S&R_SMART_LAMP-" + String(ESP.getChipId(), HEX); // SSID of access point
+		config.ssid = "GoL_Coffee_Table-" + String(ESP.getChipId(), HEX); // SSID of access point
 		config.password = "";   // password of access point
 		config.dhcp = true;
 		config.IP[0] = 192;
@@ -97,12 +94,14 @@ void setup() {
 		config.Gateway[1] = 168;
 		config.Gateway[2] = 1;
 		config.Gateway[3] = 254;
-		config.DeviceName = "S&R_SMART_LAMP";
+		config.DeviceName = "GoL_Coffee_Table";
 		config.ntpServerName = "0.ro.pool.ntp.org"; // to be adjusted to PT ntp.ist.utl.pt
 		config.Update_Time_Via_NTP_Every = 3;
 		config.timeZone = 20;
 		config.isDayLightSaving = true;
-		//aplication_configs();
+		config.density = 50;
+		config.fading_step = 30;
+		config.game = 0;
 		//WriteConfig();
 		WiFi.mode(WIFI_AP);
 		WiFi.softAP(config.ssid.c_str(),"admin1234");
@@ -145,8 +144,6 @@ void setup() {
 	server.on("/admin/ntpvalues", send_NTP_configuration_values_html);
 	server.on("/admin/timevalues", send_Time_Set_values_html);
 	server.on("/admin/gamesvalues", send_games_values_html);
-	//server.on("/admin/geolocationvalues", send_geolocation_values_html);
-	//server.on("/admin/geolocationresults", send_geolocation_results_html);
 	server.onNotFound([]() {
 		Serial.println("Page Not Found");
 		server.send ( 400, "text/html", "Page not Found" );
@@ -169,10 +166,6 @@ void setup() {
 
 	Serial.println("FastLed Setup done");
 
-	config.density = 50;
-	config.fading_step = 60;
-	config.game = 0;
-
 	// start internal time update ISR
 	tkSecond.attach(1, ISRsecondTick);
 }
@@ -185,8 +178,6 @@ void loop() {
 		if (cNTP_Update > 5 && firstStart) {
 			getNTPtime();
             delay(1500); //wait for DateTime
-			//schedule_init();
-			//user_color_init();
 			cNTP_Update = 0;
 			firstStart = false;
 		}
@@ -202,15 +193,14 @@ void loop() {
 	if (WIFI_connected != WL_CONNECTED and manual_time_set == false) {
 		config.Update_Time_Via_NTP_Every = 0;
 		//display_animation_no_wifi();
-		//softtwinkles();
-		display();
+		softtwinkles();
+		//display();
 	} else if (ntp_response_ok == false and manual_time_set == false) {
 		config.Update_Time_Via_NTP_Every = 1;
 		//display_animation_no_ntp();
-		//pride();
-		display();
+		pride();
+		//display();
 	} else if (ntp_response_ok == true or manual_time_set == true) {
-		//schedule();
 		display();
 	}
 	FastLED.show();
@@ -358,9 +348,71 @@ void chooseNewPalette() {
 		break;
 	}
 }
+//code from https://gist.github.com/kriegsman/964de772d64c502760e5
+void pride()
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+
+  for( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS-1) - pixelnumber;
+
+    nblend( leds[pixelnumber], newcolor, 64);
+  }
+}
+
+//code from https://gist.github.com/kriegsman/99082f66a726bdff7776
+const CRGB lightcolor(8,5,1);
+
+void softtwinkles() {
+  for( int i = 0; i < NUM_LEDS; i++) {
+    if( !leds[i]) continue; // skip black pixels
+    if( leds[i].r & 1) { // is red odd?
+      leds[i] -= lightcolor; // darken if red is odd
+    } else {
+      leds[i] += lightcolor; // brighten if red is even
+    }
+  }
+  // Randomly choose a pixel, and if it's black, 'bump' it up a little.
+  // Since it will now have an EVEN red component, it will start getting
+  // brighter over time.
+  if( random8() < 60) {
+    int j = random16(NUM_LEDS);
+    if( !leds[j] ) leds[j] = lightcolor;
+  }
+}
 //Games
 void conway() {
-	// Birth and death cycle - Conway's Game of Life, density 50, fading_step 6
+	// Birth and death cycle - Conway's Game of Life, density 50, fading_step 30
 	for (int x = 0; x < MatrixWidth; x++) {
 		for (int y = 0; y < MatrixHeight; y++) {
 			// Default is for cell to stay the same
